8e40bcf01ac7c92c8ca974914d8de524ad2e9194
Ronald <rn2702@gmail.com>
2024-10-14 11:09:40 -0400

    Added js file

---
+++ TeraFrontendMVC/wwwroot/js/results-search.js
@@ -0,0 +1,15 @@
+﻿function resultsSearch() {
+    document.getElementById("btnBuscar").addEventListener("click", function () {
+        var codEdo = document.getElementById("selectEstado").value;
+        var codMun = document.getElementById("selectMunicipio").value;
+        var codPar = document.getElementById("selectParroquia").value;
+
+        // Llama al controlador
+        fetch(`/Results/Buscar?codEdo=${codEdo}&codMun=${codMun}&codPar=${codPar}`)
+            .then(response => response.text()) // Cambiado a 'text()' para manejar HTML
+            .then(data => {
+                document.getElementById("resultadosParciales").innerHTML = data;
+            })
+            .catch(error => console.error('Error:', error));
+    });
+}
