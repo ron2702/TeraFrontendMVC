ea13fd46362fb1dbc2fc4eefadd74bbfede815a3
Ronald <rn2702@gmail.com>
2024-11-06 12:27:05 -0400

    Added Login View

---
+++ TeraFrontendMVC/Controllers/AccountController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TeraFrontendMVC.Models.Account;
+
+namespace TeraFrontendMVC.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account/Login
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        // POST: Account/Login
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Login(Login model)
+        {
+            if (ModelState.IsValid)
+            {
+                // Lógica de autenticación aquí
+                return RedirectToAction("Index", "Home"); // Redirige a la página principal si es exitoso
+            }
+
+            return View(model); // Si hay errores, vuelve a mostrar el formulario
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Register(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult ChangePassword(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
