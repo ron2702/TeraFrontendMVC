ab50027642c4c941b20c9c9f1413aac2127c3c16
Ronald <rn2702@gmail.com>
2024-11-07 12:26:55 -0400

    Added changePwd endpoint and logic

--- TeraFrontendMVC/Controllers/AccountController.cs
+++ TeraFrontendMVC/Controllers/AccountController.cs
@@ -47,6 +47,37 @@ namespace TeraFrontendMVC.Controllers
             return View(model);
         }
 
+        // GET: Account/ChangePassword
+        [HttpGet]
+        public IActionResult ChangePassword()
+        {
+            return View();
+        }
+
+        // POST: Account/Register
+        [HttpPost]
+        public async Task<IActionResult> ChangePassword(ChangePassword model)
+        {
+            if (ModelState.IsValid)
+            {
+                var jsonContent = JsonConvert.SerializeObject(model);
+                var content = new StringContent(jsonContent, Encoding.UTF8, "application/json");
+                var response = await _httpClient.PostAsync("http://web/api/Usuarios/modificar-contrasena", content);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    return RedirectToAction("Login", "Account");
+                }
+
+                var errorMessage = await response.Content.ReadAsStringAsync();
+                ModelState.AddModelError(string.Empty, errorMessage);
+            }
+
+            // Asegúrate de devolver el modelo al regresar a la vista
+            return View(model);
+        }
+
+
         // GET: Account/Login
         [HttpGet]
         public IActionResult Login()
@@ -57,25 +88,6 @@ namespace TeraFrontendMVC.Controllers
         // POST: Account/Login
         [HttpPost]
         public IActionResult Login(Login model)
-        {
-            if (ModelState.IsValid)
-            {
-                // Lógica de autenticación aquí
-                return RedirectToAction("Index", "Home"); // Redirige a la página principal si es exitoso
-            }
-
-            return View(model); // Si hay errores, vuelve a mostrar el formulario
-        }
-
-        // GET: Account/Register
-        [HttpGet]
-        public IActionResult ChangePassword()
-        {
-            return View();
-        }
-
-        [HttpPost]
-        public IActionResult ChangePassword(ChangePassword model)
         {
             if (ModelState.IsValid)
             {
