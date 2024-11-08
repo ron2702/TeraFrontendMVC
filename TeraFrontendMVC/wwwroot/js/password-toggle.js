3d525c66c20d731e0f43f69cab516e8904c23139
Ronald <rn2702@gmail.com>
2024-11-08 12:15:01 -0400

    Added eye icon toggle in password fields

---
+++ TeraFrontendMVC/wwwroot/js/password-toggle.js
@@ -0,0 +1,18 @@
+﻿// password-toggle.js
+function togglePasswordVisibility(inputId, iconId) {
+    const passwordInput = document.getElementById(inputId);
+    const toggleIcon = document.getElementById(iconId);
+
+    if (passwordInput && toggleIcon) {
+        toggleIcon.addEventListener('click', function () {
+            const type = passwordInput.type === 'password' ? 'text' : 'password'; // Alterna entre 'password' y 'text'
+            passwordInput.type = type;
+
+            // Cambiar el ícono dependiendo del tipo
+            this.classList.toggle('fa-eye-slash');
+        });
+    }
+}
+
+// Llama a esta función para cada campo de contraseña que necesite el toggle
+// togglePasswordVisibility('passwordInput', 'togglePassword');
